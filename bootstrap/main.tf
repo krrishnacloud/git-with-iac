14a1ff915a10077d9479b81a2932f4bb3bea80b1
krrishnacloud <krishnavemula423@gmail.com>
2026-03-05 17:53:03 +0530

    dynamodb added

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -10,3 +10,14 @@ resource "aws_s3_bucket" "s3_bucket" {
 
   }
 }
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
--- terraform-files/backend.tf
+++ terraform-files/backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "git-with-terraform"
     key = "terraform-file/terraform.tfstate"
     region = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
 }
