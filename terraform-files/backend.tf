43aab34253e40ba347261c3707ba2bc89e846e3e
krrishnacloud <krishnavemula423@gmail.com>
2026-03-05 12:15:49 +0530

    bucket name updated

--- terraform-files/backend.tf
+++ terraform-files/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "git with terraform"
+    bucket = "git_with_terraform"
     key = "terraform-file/terraform.tfstate"
     region = "ap-south-1"
     encrypt = true
