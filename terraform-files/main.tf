4c23cf80a895a2513694d81f6921644ab922ff10
krrishnacloud <krishnavemula423@gmail.com>
2026-03-05 13:35:37 +0530

    var.instance_type modified

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "instance" {
   ami = "var.ami"
-  instance_type = var.instance_type
+  instance_type = "var.instance_type"
 
   tags = {
     Name= "git with iac"
