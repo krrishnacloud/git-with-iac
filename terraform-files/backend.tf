7dfb2d4ef900375ac2eeac746d7f8600da6bcf96
krrishnacloud <krishnavemula423@gmail.com>
2026-03-05 12:30:53 +0530

    remmoved dynamodb line

--- terraform-files/backend.tf
+++ terraform-files/backend.tf
@@ -3,8 +3,5 @@ terraform {
     bucket = "git_with_terraform"
     key = "terraform-file/terraform.tfstate"
     region = "ap-south-1"
-    encrypt = true
-    dynamodb_table = "terraform-lock-table"
   }
-
 }
